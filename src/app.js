3eb5df39eeb26042796587852a8e83d32f898096
letidega <degaloveless@hotmail.com>
2022-09-22 09:31:35 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excuse").innerHTML = MyExcuse();
   console.log("Hello Rigo from the console!");
 };
+
+function MyExcuse() {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
